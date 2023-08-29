0a96225f35c43b96befa0308aa47ebca6619043b
vadim <gabderahmanov99@gmail.com>
2023-08-29 13:26:35 +0300

    MainViewModel.kt init, stateflow for handle result

---
+++ app/src/main/java/com/abg/testapp/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.abg.testapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.abg.testapp.data.MainRepository
+import com.abg.testapp.data.Resource
+import com.abg.testapp.model.Camera
+import com.abg.testapp.model.Door
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+//ViewModel for handle result from repository
+class MainViewModel(repository: MainRepository) : ViewModel() {
+
+    private val _doors = MutableStateFlow<Resource<List<Door>?>?>(null) // we can get null from response
+    val doors: StateFlow<Resource<List<Door>?>?> = _doors
+
+    private val _cameras = MutableStateFlow<Resource<List<Camera>?>?>(null)
+    val cameras: StateFlow<Resource<List<Camera>?>?> = _cameras
+
+    init {
+        viewModelScope.launch {
+            _doors.value = Resource.Loading
+            _doors.value = repository.getDoors()
+
+            _cameras.value = Resource.Loading
+            _cameras.value = repository.getCameras()
+        }
+    }
+}
+
+class MainViewModelFactory(private val repository: MainRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
+}
