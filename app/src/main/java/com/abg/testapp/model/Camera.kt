fc8212c1fb345d63e9f1f0cbf865de53374e8a8f
vadim <gabderahmanov99@gmail.com>
2023-08-29 13:05:39 +0300

    Camera.kt init

---
+++ app/src/main/java/com/abg/testapp/model/Camera.kt
@@ -0,0 +1,28 @@
+package com.abg.testapp.model
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Camera: RealmObject {
+    @PrimaryKey
+    @SerialName("id")
+    var id: Int = 0
+
+    @SerialName("name")
+    var name: String = ""
+
+    @SerialName("snapshot")
+    var snapshot: String = ""
+
+    @SerialName("room")
+    var room: String? = null
+
+    @SerialName("favorites")
+    var favorites: Boolean = false
+
+    @SerialName("rec")
+    var rec: Boolean = false
+}
