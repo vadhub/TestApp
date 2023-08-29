be74421116ac435a9b7b1fa664d447ef07bfd3b9
vadim <gabderahmanov99@gmail.com>
2023-08-29 13:35:10 +0300

    TabLayout.kt

---
+++ app/src/main/java/com/abg/testapp/ui/TabLayout.kt
@@ -0,0 +1,58 @@
+package com.abg.testapp.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout(cameraScreen: @Composable () -> Unit, doorScreen: @Composable () -> Unit) {
+    val tabsList = listOf("Doors", "Cameras")
+    val pagerState = rememberPagerState(tabsList.size)
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { pos ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                )
+            },
+            backgroundColor = Color.White,
+            contentColor = Color.Blue
+        ) {
+
+            tabsList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = { Text(text = text) }
+                )
+            }
+        }
+
+        HorizontalPager(state = pagerState, modifier = Modifier.weight(1f)) { index ->
+            when (index) {
+                0 -> cameraScreen.invoke()
+                1 -> doorScreen.invoke()
+            }
+        }
+    }
+}
