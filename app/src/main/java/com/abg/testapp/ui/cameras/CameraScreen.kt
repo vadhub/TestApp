8593d72872def0d0d94f60f9623eb9a658b3e302
vadim <gabderahmanov99@gmail.com>
2023-08-29 13:33:29 +0300

    CameraScreen.kt init

---
+++ app/src/main/java/com/abg/testapp/ui/cameras/CameraScreen.kt
@@ -0,0 +1,12 @@
+package com.abg.testapp.ui.cameras
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.abg.testapp.MainViewModel
+
+@Composable
+fun CamerasScreen(viewModel: MainViewModel) {
+    Column {
+
+    }
+}
