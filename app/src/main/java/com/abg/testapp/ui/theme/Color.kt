d6a878f8ef2044763908b65cc0ac67f1382e1c5c
vadim <gabderahmanov99@gmail.com>
2023-08-30 09:57:35 +0300

    color set in theme

--- app/src/main/java/com/abg/testapp/ui/theme/Color.kt
+++ app/src/main/java/com/abg/testapp/ui/theme/Color.kt
@@ -5,6 +5,10 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val Beige = Color(0xFFF5F5F5)
+val BeigeDark = Color(0xFFD3D3D3)
+val Yellow = Color(0xFFB39F00)
+val Blue = Color(0xFF2BB5FF)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
