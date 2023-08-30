2ecd419c00cf2726a6777aa3d1ff60d31aced644
vadim <gabderahmanov99@gmail.com>
2023-08-30 10:00:06 +0300

    check empty db, insert, update in viewmodel

--- app/src/main/java/com/abg/testapp/MainViewModel.kt
+++ app/src/main/java/com/abg/testapp/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.abg.testapp
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -9,10 +10,11 @@ import com.abg.testapp.model.Camera
 import com.abg.testapp.model.Door
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 //ViewModel for handle result from repository
-class MainViewModel(repository: MainRepository) : ViewModel() {
+class MainViewModel(private val repository: MainRepository) : ViewModel() {
 
     private val _doors = MutableStateFlow<Resource<List<Door>?>?>(null) // we can get null from response
     val doors: StateFlow<Resource<List<Door>?>?> = _doors
@@ -20,15 +22,60 @@ class MainViewModel(repository: MainRepository) : ViewModel() {
     private val _cameras = MutableStateFlow<Resource<List<Camera>?>?>(null)
     val cameras: StateFlow<Resource<List<Camera>?>?> = _cameras
 
-    init {
-        viewModelScope.launch {
+    fun getDoors() = viewModelScope.launch {
+        if (repository.checkIsEmptyDoorDB()) {
             _doors.value = Resource.Loading
             _doors.value = repository.getDoorsFromRemote()
-
-            _cameras.value = Resource.Loading
-            _cameras.value = repository.getCamerasFromRemote()
+            Log.d("connect", "getDoors: ")
+            _doors.collectLatest { list ->
+                if (list is Resource.Success) {
+                    list.result?.let { repository.insertAllDoors(it) }
+                }
+            }
+        } else {
+            Log.d("db", "getDoors: ")
+            _doors.value = Resource.Loading
+            repository.getDoorsFromDB().collectLatest {
+                _doors.value = Resource.Success(it)
+            }
         }
     }
+
+    fun insertFavoriteDoor(door: Door) = viewModelScope.launch {
+        repository.insertOrUpdateFavoriteDoor(door)
+    }
+
+    fun insertRenamedDoor(door: Door) = viewModelScope.launch {
+        repository.insertOrUpdateRenamedDoor(door)
+    }
+
+    fun getCameras() = viewModelScope.launch {
+        if (repository.checkIsEmptyCameraDB()) {
+            _cameras.value = Resource.Loading
+            _cameras.value = repository.getCamerasFromRemote()
+            Log.d("connect", "getCameras: ")
+            _cameras.collectLatest { list ->
+                if (list is Resource.Success) {
+                    list.result?.let { repository.insertAllCameras(it) }
+                }
+            }
+        } else {
+            Log.d("db", "getCameras: ")
+            _cameras.value = Resource.Loading
+            repository.getCamerasFromDB().collectLatest {
+                _cameras.value = Resource.Success(it)
+            }
+        }
+    }
+
+    fun insertFavoriteCamera(camera: Camera) = viewModelScope.launch {
+        repository.insertOrUpdateFavoriteCamera(camera)
+    }
+
+    fun insertRenamedCamera(camera: Camera) = viewModelScope.launch {
+        repository.insertOrUpdateRenamedCamera(camera)
+    }
+
 }
 
 class MainViewModelFactory(private val repository: MainRepository): ViewModelProvider.Factory {
