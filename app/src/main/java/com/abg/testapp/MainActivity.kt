9659e7d97a4013add6818d682d8b63c8b8bce046
vadim <gabderahmanov99@gmail.com>
2023-08-30 10:02:37 +0300

    init screens in main activity

--- app/src/main/java/com/abg/testapp/MainActivity.kt
+++ app/src/main/java/com/abg/testapp/MainActivity.kt
@@ -3,44 +3,80 @@ package com.abg.testapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.abg.testapp.data.DatabaseInstance
+import com.abg.testapp.data.MainRepository
+import com.abg.testapp.data.RemoteInstance
+import com.abg.testapp.model.Camera
+import com.abg.testapp.model.Door
+import com.abg.testapp.ui.TabLayout
+import com.abg.testapp.ui.cameras.CamerasScreen
+import com.abg.testapp.ui.doors.DoorsScreen
+import com.abg.testapp.ui.theme.Beige
 import com.abg.testapp.ui.theme.TestAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel> {
+        MainViewModelFactory(
+            MainRepository(
+                RemoteInstance.client(),
+                DatabaseInstance.provideRealm()
+            )
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.getDoors()
+        viewModel.getCameras()
+
+        val favoriteCamera: (Camera) -> Unit = {
+            viewModel.insertFavoriteCamera(it)
+        }
+
+        val editCamera: (Camera) -> Unit = {
+            viewModel.insertFavoriteCamera(it)
+        }
+
+        val favoriteDoor: (Door) -> Unit = {
+            viewModel.insertFavoriteDoor(it)
+        }
+
+        val editDoor: (Door) -> Unit = {
+            viewModel.insertRenamedDoor(it)
+        }
+
         setContent {
             TestAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column(Modifier.background(Beige)) {
+                    TopBar()
+                    TabLayout(
+                        { DoorsScreen(doors = viewModel.doors.collectAsState(), favoriteDoor, editDoor) },
+                        { CamerasScreen(cameras = viewModel.cameras.collectAsState(), favoriteCamera, editCamera)})
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TopBar() {
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Beige)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestAppTheme {
-        Greeting("Android")
-    }
-}
